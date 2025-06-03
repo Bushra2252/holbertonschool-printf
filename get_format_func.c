bc33275da99c33cd38b98e14cecc1067abe76bd5
RaghadAlbeladi1 <Raghadz_5@hotmail.com>
2025-06-03 08:40:59 +0000

    add get_format_func.c

--- get_format_func.c
+++ get_format_func.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * get_format_func - finds the correct print function for specifier
- * @c: format specifier character
- * Return: pointer to corresponding function, or NULL if not found
- */
+* get_format_func - finds the correct print function for specifier
+* @c: format specifier character
+* Return: pointer to corresponding function, or NULL if not found
+*/
 int (*get_format_func(char c))(va_list)
 {
- specifier_type formats[] = {
-  {'c', print_char},
-  {'s', print_string},
-  {'d', print_integer},
-  {'i', print_integer},
-  {'%', print_percent},
-  {0, NULL}
- };
- int i = 0;
+specifier_type formats[] = {
+{'c', print_char},
+{'s', print_string},
+{'d', print_integer},
+{'i', print_integer},
+{'%', print_percent},
+{0, NULL}
+};
+int i = 0;
 
- while (formats[i].spec)
- {
-  if (formats[i].spec == c)
-   return (formats[i].func);
-  i++;
- }
- return (NULL);
+while (formats[i].spec)
+{
+if (formats[i].spec == c)
+return (formats[i].func);
+i++;
+}
+return (NULL);
 }
