41147a7499475d5372e5c798f28e89ce5c6ef4cd
RaghadAlbeladi1 <Raghadz_5@hotmail.com>
2025-06-03 08:47:18 +0000

    add _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - print a char to stdout
- * @c: char to print
- * Return: 1
- */
+* _putchar - print a char to stdout
+* @c: char to print
+* Return: 1
+*/
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+return (write(1, &c, 1));
 }
